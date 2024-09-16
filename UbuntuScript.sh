8b7ca73e0cef4317e1e6fe52ca778857dfc70a82
X-Xydley <xydleydiscord@gmail.com>
2024-09-16 09:33:53 -0400

    Update UbuntuScript.sh

--- UbuntuScript.sh
+++ UbuntuScript.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # update system
-apt-get update 
-apt-get upgrade 
-apt-get dist-upgrade
+sudo apt-get update 
+sudo apt-get upgrade 
+sudo apt-get dist-upgrade
 
 # secure root
-sed -i 's/PermitRootLogin yes/PermitRootLogin no' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sudo service ssh reload
+
+# enfore password reqs
+sudo sed -i 's/PASS_MAX_DAYS.*/PASS_MAX_DAYS    90/' /etc/login.defs
+sudo sed -i 's/PASS_MIN_DAYS.*/PASS_MIN_DAYS    7/' /etc/login.defs
+sudo sed -i 's/PASS_WARN_AGE.*/PASS_WARN_AGE    14/' /etc/login.defs
+
+# enable firewall
+sudo ufw enable
