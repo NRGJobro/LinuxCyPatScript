3478fd9bb390b1544eedfd2e699559dcfb2ab199
X-Xydley <xydleydiscord@gmail.com>
2024-09-20 12:31:33 -0400

    Update UbuntuScript.sh

--- UbuntuScript.sh
+++ UbuntuScript.sh
@@ -5,12 +5,6 @@ sudo apt-get update
 sudo apt-get upgrade 
 sudo apt-get dist-upgrade
 
-# delete users
-#sudo userdel -r [USER]
-
-# remove admin perms
-#sudo deluser [USER] sudo
-
 # change user password
 #sudo passwd [USER]
 
@@ -26,4 +20,5 @@ sudo sed -i 's/PASS_WARN_AGE.*/PASS_WARN_AGE    14/' /etc/login.defs
 # enable firewall
 sudo ufw enable
 
-# sudo sed -i 'pam_unix.so/s/$/ minlen=8 remember=5' /etc/pam.d/common-password 
+# min password length and password history
+sudo sed -i '/pam_unix.so/ s/$/ minlen=8 remember=5/' /etc/pam.d/common-password
